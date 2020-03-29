0294381a8fc7a9f69be72491bcf3a84f9c5ec9d3
Eduardo Souza <edux404@gmail.com>
2020-03-29 17:39:47 -0300

    [ChartEntryStat] Add NoDiff value

--- src/ThreeChartsAPI/Models/ChartEntryStat.cs
+++ src/ThreeChartsAPI/Models/ChartEntryStat.cs
@@ -1,4 +1,4 @@
 namespace ThreeChartsAPI.Models
 {
-    public enum ChartEntryStat { New, Reentry, Increase, Decrease }
+    public enum ChartEntryStat { New, Reentry, NoDiff, Increase, Decrease }
 }
