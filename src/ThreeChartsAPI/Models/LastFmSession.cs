78c190eebf35bb884ad8df23714f83742e442622
Eduardo Souza <edux404@gmail.com>
2020-03-30 03:19:39 -0300

    [ILastFmService] Add CreateLastFmSession method

---
+++ src/ThreeChartsAPI/Models/LastFmSession.cs
@@ -0,0 +1,8 @@
+namespace ThreeChartsAPI.Models.LastFm
+{
+    public class LastFmSession
+    {
+        public string LastFmUser { get; set; } = null!;
+        public string Key { get; set; } = null!;
+    }
+}
--- src/ThreeChartsAPI/Services/ILastFmService.cs
+++ src/ThreeChartsAPI/Services/ILastFmService.cs
@@ -5,6 +5,7 @@ namespace ThreeChartsAPI.Services.LastFm
 {
     public interface ILastFmService
     {
+        Task<LastFmSession> CreateLastFmSession(string token);
         Task<LastFmChart<LastFmChartTrack>> GetWeeklyTrackChart(string user, long from, long to);
         Task<LastFmChart<LastFmChartAlbum>> GetWeeklyAlbumChart(string user, long from, long to);
         Task<LastFmChart<LastFmChartArtist>> GetWeeklyArtistChart(string user, long from, long to);
