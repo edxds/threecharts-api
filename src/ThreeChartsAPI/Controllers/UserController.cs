c999f648137df949dfaecf8d85288cdaa9352f5f
Eduardo Souza <edux404@gmail.com>
2020-04-02 05:13:57 -0300

    [UserController] Add Onboard method

--- src/ThreeChartsAPI/Controllers/UserController.cs
+++ src/ThreeChartsAPI/Controllers/UserController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +7,7 @@ using ThreeChartsAPI.Models.LastFm;
 using ThreeChartsAPI.Models.LastFm.Dtos;
 using ThreeChartsAPI.Services;
 using ThreeChartsAPI.Services.LastFm;
+using ThreeChartsAPI.Services.Onboarding;
 
 namespace ThreeChartsAPI.Controllers
 {
@@ -15,11 +17,16 @@ namespace ThreeChartsAPI.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly IOnboardingService _onboardingService;
         private readonly ILastFmService _lastFm;
 
-        public UserController(IUserService userService, ILastFmService lastFmService)
+        public UserController(
+            IUserService userService,
+            IOnboardingService onboardingService,
+            ILastFmService lastFmService)
         {
             _userService = userService;
+            _onboardingService = onboardingService;
             _lastFm = lastFmService;
         }
 
@@ -56,6 +63,39 @@ namespace ThreeChartsAPI.Controllers
                 ProfilePicture = user.ProfilePicture,
                 RegisteredAt = user.RegisteredAt,
             });
+        }
+
+        [HttpPost]
+        [Route("onboard")]
+        public async Task<ActionResult> Onboard()
+        {
+            var userName = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if (userName == null)
+            {
+                return Unauthorized();
+            }
+
+            var user = await _userService.FindUserFromUserName(userName);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var onboardingResult = await _onboardingService.OnboardUser(user, null);
+            if (onboardingResult.IsFailed)
+            {
+                var lastFmError = onboardingResult.Errors.Find(error =>
+                    error is LastFmResultError
+                ) as LastFmResultError;
+
+                return StatusCode(lastFmError!.StatusCode, new LastFmErrorDto()
+                {
+                    ErrorCode = lastFmError!.LastFmErrorCode ?? -1,
+                    Message = lastFmError!.LastFmErrorMessage ?? "Last.fm service unavailable."
+                });
+            }
+
+            return NoContent();
         }
     }
 }
