e0d4b366284829242de2bff92d2e8e352863f496
Eduardo Souza <edux404@gmail.com>
2020-04-23 00:05:47 -0300

    [ChartController] Add get live week route

--- src/ThreeChartsAPI/Features/Charts/ChartController.cs
+++ src/ThreeChartsAPI/Features/Charts/ChartController.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ThreeChartsAPI.Features.Charts.Dtos;
 using ThreeChartsAPI.Features.Charts.Models;
+using ThreeChartsAPI.Features.LastFm;
+using ThreeChartsAPI.Features.LastFm.Dtos;
+using ThreeChartsAPI.Features.Users;
 using ThreeChartsAPI.Features.Users.Dtos;
 
 namespace ThreeChartsAPI.Features.Charts
@@ -15,10 +20,17 @@ namespace ThreeChartsAPI.Features.Charts
     public class ChartController : ControllerBase
     {
         private readonly ChartRepository _repo;
+        private readonly IChartService _chartService;
+        private readonly IUserService _userService;
 
-        public ChartController(ChartRepository repo)
+        public ChartController(
+            ChartRepository repo,
+            IChartService chartService,
+            IUserService userService)
         {
             _repo = repo;
+            _chartService = chartService;
+            _userService = userService;
         }
 
         [HttpGet("weeks/{ownerId}")]
@@ -51,8 +63,50 @@ namespace ThreeChartsAPI.Features.Charts
             {
                 return NotFound();
             }
+            
+            return Ok(MakeChartsDtoFrom(week));
+        }
 
-            var chartsDto = new ChartsDto()
+        [HttpGet("live")]
+        public async Task<ActionResult<ChartsDto>> GetLiveChartForUser()
+        {
+            var userName = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if (userName == null)
+            {
+                return Unauthorized();
+            }
+
+            var user = await _userService.FindUserFromUserName(userName);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            if (user.IanaTimezone == null)
+            {
+                return BadRequest();
+            }
+            
+            var week = await _chartService.GetLiveWeekFor(user, DateTime.UtcNow);
+            
+            if (week.IsFailed)
+            {
+                var lastFmError = week.Errors.Find(error =>
+                    error is LastFmResultError
+                ) as LastFmResultError;
+
+                return StatusCode(lastFmError!.StatusCode, new LastFmErrorDto()
+                {
+                    ErrorCode = lastFmError!.LastFmErrorCode ?? -1,
+                    Message = lastFmError!.LastFmErrorMessage ?? "Last.fm service unavailable."
+                });
+            }
+            
+            return Ok(MakeChartsDtoFrom(week.Value));
+        }
+        
+        private ChartsDto MakeChartsDtoFrom(ChartWeek week) =>
+            new ChartsDto
             {
                 OwnerId = week.OwnerId,
                 WeekId = week.Id,
@@ -61,7 +115,7 @@ namespace ThreeChartsAPI.Features.Charts
                 TrackEntries = week.ChartEntries
                     .Where(entry => entry.Type == ChartEntryType.Track)
                     .OrderBy(entry => entry.Rank)
-                    .Select(entry => new TrackEntryDto()
+                    .Select(entry => new TrackEntryDto
                     {
                         Id = entry.Id,
                         Rank = entry.Rank,
@@ -76,7 +130,7 @@ namespace ThreeChartsAPI.Features.Charts
                 AlbumEntries = week.ChartEntries
                     .Where(entry => entry.Type == ChartEntryType.Album)
                     .OrderBy(entry => entry.Rank)
-                    .Select(entry => new AlbumEntryDto()
+                    .Select(entry => new AlbumEntryDto
                     {
                         Id = entry.Id,
                         Rank = entry.Rank,
@@ -91,7 +145,7 @@ namespace ThreeChartsAPI.Features.Charts
                 ArtistEntries = week.ChartEntries
                     .Where(entry => entry.Type == ChartEntryType.Artist)
                     .OrderBy(entry => entry.Rank)
-                    .Select(entry => new ArtistEntryDto()
+                    .Select(entry => new ArtistEntryDto
                     {
                         Id = entry.Id,
                         Stat = entry.Stat,
@@ -102,8 +156,5 @@ namespace ThreeChartsAPI.Features.Charts
                     })
                     .ToList(),
             };
-
-            return Ok(chartsDto);
-        }
     }
 }
